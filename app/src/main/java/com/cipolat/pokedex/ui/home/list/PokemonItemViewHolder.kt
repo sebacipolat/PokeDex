1e4b2e844808287ee915602c83f4bb6e5f319373
sebastiancipolat <sebastiancipolat@gmail.com>
2020-11-02 09:36:56 -0300

    get color backgrosund from image

--- app/src/main/java/com/cipolat/pokedex/ui/home/list/PokemonItemViewHolder.kt
+++ app/src/main/java/com/cipolat/pokedex/ui/home/list/PokemonItemViewHolder.kt
@@ -1,13 +1,22 @@
 package com.cipolat.pokedex.ui.home.list
 
+import android.R
+import android.R.attr
+import android.graphics.drawable.Drawable
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.cipolat.pokedex.data.model.Pokemon
 import kotlinx.android.synthetic.main.pokemon_itm.view.*
 
+
 class PokemonItemViewHolder(
-        itemView: View
+    itemView: View
 ) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
     private var view: View = itemView
@@ -19,8 +28,22 @@ class PokemonItemViewHolder(
 
     fun bindItem(pokemon: Pokemon) {
         this.pokemon = pokemon
-        view.name_lbl.text=pokemon.name.capitalize()
+        view.name_lbl.text = pokemon.name.capitalize()
         Glide.with(view.context).load(pokemon.url.getPokeImage()).into(view.icon);
+        Glide.with(view.context).load(pokemon.url.getPokeImage()).into(object :
+            CustomTarget<Drawable>() {
+            override fun onLoadCleared(placeholder: Drawable?) {
+
+            }
+
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                view.icon.setImageDrawable(resource)
+                Palette.from(resource.toBitmap()).generate { p ->
+                    val color: Int? =p?.getLightVibrantColor(ContextCompat.getColor(view.context,R.color.darker_gray))
+                    color?.let { view.card.setCardBackgroundColor(it) }
+                }
+            }
+        });
     }
 
     override fun onClick(view: View?) {
--- app/src/main/java/com/cipolat/pokedex/ui/home/list/PokemonUrlExtension.kt
+++ app/src/main/java/com/cipolat/pokedex/ui/home/list/PokemonUrlExtension.kt
@@ -6,6 +6,3 @@ fun String.getPokeImage():String{
     val number=String.format("%03d", img)
     return "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/$number.png"
 }
-
-
-
